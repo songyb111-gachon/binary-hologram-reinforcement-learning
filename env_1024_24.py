8e57653027f348f7a2962990366d69c91eb1565c
songyb111-gachon <songyb111@gachon.ac.kr>
2025-01-17 22:04:12 +0900

    dbs

--- env_1024_24.py
+++ env_1024_24.py
@@ -196,7 +196,7 @@ class BinaryHologramEnv(gym.Env):
 
         print(f"RGB shape: {rgb.shape}, dtype: {rgb.dtype}, device: {rgb.device}")
 
-        print(f"target image shape: {self.target_image.shape}, dtype: {{self.target_image.dtype}, device: {{self.target_image.device}")
+        print(f"target image shape: {self.target_image.shape}, dtype: {self.target_image.dtype}, device: {self.target_image.device}")
 
         # 타겟 이미지와 RGB 결과의 차원 일치 확인
         if rgb.shape != self.target_image.shape:
